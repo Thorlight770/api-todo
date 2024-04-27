c62775c256fa2ed2de93ad7a6e1c7f762bb13673
Thorlight770 <gunawandr7@gmail.com>
2024-04-27 07:57:06 +0700

    [User] add repository

---
+++ api.todo/Repository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using api.todo.Model;
+
+namespace api.todo.Repository
+{
+    public interface IUserRepository
+    {
+        #region Inquiry
+        public Task<User> GetById(string id);
+        public Task<User> Login(string username, string password);
+        #endregion
+
+        #region CRUD
+        public User Add(User user);
+        public User Update(User user);
+        public bool Delete(string id);
+        #endregion
+    }
+}
---
+++ api.todo/Repository/Impl/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using api.todo.Context;
+using api.todo.Model;
+using System.Linq;
+
+namespace api.todo.Repository.Impl
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserContext _context;
+
+        public UserRepository(UserContext context)
+        {
+            _context = context;
+        }
+
+        public User Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetById(string id)
+        {
+            return await _context.User.FindAsync(id);
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            return _context.User.Select(x => x).Where(x => x.Email == username && x.Password == password).SingleOrDefault();
+        }
+
+        public User Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

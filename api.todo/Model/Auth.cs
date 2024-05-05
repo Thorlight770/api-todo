a92d8bb936521742ea0aa4f08094d77fe79fc5fa
Thorlight770 <gunawandr7@gmail.com>
2024-05-05 10:19:07 +0700

    [ModelValidation] add require and custom message error

--- api.todo/Model/Auth.cs
+++ api.todo/Model/Auth.cs
@@ -1,5 +1,8 @@
-﻿namespace api.todo.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.todo.Model
 {
+    // model validation https://learn.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-6.0
     public class Auth
     {
         public string? Token { get; set; }
@@ -7,7 +10,10 @@
 
     public class AuthRq
     {
+        [Required(ErrorMessage = "Username is null or empty !")]
         public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is null or empty !")]
         public string? Password { get; set; }
     }
 }
--- api.todo/Model/User.cs
+++ api.todo/Model/User.cs
@@ -1,9 +1,11 @@
 ﻿using api.todo.Model.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.todo.Model
 {
     public class User
     {
+        [Required(ErrorMessage = "ID can't is null or empty !")]
         public string? ID { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
